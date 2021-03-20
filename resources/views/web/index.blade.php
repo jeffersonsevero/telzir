30babb7cc363d0b4dc12fb91386f44d50d6cc98e
Jefferson Severo <jeffersonsevero08@gmail.com>
2021-03-19 22:17:32 -0300

    front almost complete

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -6,8 +6,41 @@
 
 <link rel="stylesheet" href="{{ public_path() . '/css/custom.css'  }}" />
 
+
+
+<section>
+
+    <div class="container py-5">
+
+        <div class="row">
+
+            <div class="col-md-6 py-3">
+
+                <h1 class="display-3 text-primary text-bold">Venha ser da família Telzir</h1>
+                <h5>Conheça os nossos planos e vantagens e fale o quanto quiser pelo menor custo!</h5>
+
+            </div>
+            <div class="col-md-6">
+
+                <img id="image-capa" src="{{ url('images/cel.svg') }}" alt="">
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</section>
+
 <section class="pricing py-5">
     <div class="container">
+
+
+
+
+
         <div class="row">
 
 
@@ -17,18 +50,19 @@
             @foreach ($plans as $plan )
 
             <!-- Pro Tier -->
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title text-muted text-uppercase text-center">{{ $plan->name }}</h5>
                         <hr>
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong> Fale grátis por {{ $plan->minutes }}</strong>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong> Fale grátis por
+                                    {{ $plan->minutes }}</strong>
                             </li>
-                            <li><span class="fa-li"><i class="fas fa-check"></i></span> Apenas 10% sobre a taxa depois dos {{ $plan->minutes }} </li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span> Apenas 10% sobre a taxa depois
+                                dos {{ $plan->minutes }} </li>
 
                         </ul>
-                        <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
                     </div>
                 </div>
             </div>
@@ -50,6 +84,114 @@
 </section>
 
 
+
+<section>
+
+    <div class="container py-5">
+
+        <h1 class="text-center text-primary ">Faça agora a sua simulação</h1>
+
+
+        <div class="row">
+
+
+
+            <div class="col-md-6 py-3">
+
+
+
+                <form action="" method="post">
+
+                    <div class="form-group">
+                        <label class="text-primary text-uppercase" for="origin">Origem</label>
+                        <select class="form-control" name="origin" id="origin">
+                            <option value="">01</option>
+                            <option value="">01</option>
+                            <option value="">01</option>
+                            <option value="">01</option>
+                        </select>
+
+                    </div>
+
+
+                    <div class="form-group">
+                        <label class="text-primary text-uppercase" for="destiny">Destino</label>
+                        <select class="form-control" name="destiny" id="destiny">
+                            <option value="">01</option>
+                            <option value="">01</option>
+                            <option value="">01</option>
+                            <option value="">01</option>
+                        </select>
+
+                    </div>
+
+
+                    <div class="form-group">
+
+                        <label for="time">Tempo em minutos</label>
+                        <input type="number" id="time" name="time" class="form-control">
+
+
+                    </div>
+
+
+                    <div class="form-group">
+
+                        @foreach ($plans as $plan)
+
+                        <input type="radio" name="plan" id="plan">
+                        <label class="mr-3" for="plan"> {{ $plan->name }} </label>
+
+
+                        @endforeach
+
+                    </div>
+
+                    <input type="submit" value="Simular" class="btn btn-lg btn-primary">
+
+                </form>
+
+
+
+
+            </div>
+            <div class="col-md-6">
+
+                <div class="ajax_load">
+                    <div class="ajax_load_box">
+                        <div class="ajax_load_box_circle"></div>
+                        <div class="ajax_load_box_title">Aguarde, carrengando...</div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</section>
+
+
+
+
+
+@endsection
+
+
+
+
+@section('js')
+
+<script>
+
+
+
+
+
+</script>
 
 
 @endsection
--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -11,8 +11,10 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
 
-
+    <link rel="stylesheet" href="{{ url('css/custom.css')   }}">
 
 
 </head>
@@ -20,6 +22,32 @@
 <body>
 
 
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#corNavbar01"
+            aria-controls="corNavbar01" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="corNavbar01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Destaques</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Preços</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
 
 
     @yield('content')
@@ -32,11 +60,20 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
+
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+        integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 
+
     <script src="{{ public_path() . '/js/script.js'  }}"></script>
+
+    @hasSection('js')
+    @yield('js')
+
+    @endif
 
 
 </body>
